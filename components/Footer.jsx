dae50194b4e085fc102efd803378f922f8e59229
lgorka <110176770+Cinamon88@users.noreply.github.com>
2024-02-12 14:20:48 +0100

    add footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,18 @@
+import Socials from "./Socials"
 
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer className="bg-primary/10 dark:bg-secondary py-12">
+      <div className="container mx-auto">
+        <div className="flex flex-col items-center justify-between">
+          <Socials 
+            containerStyles='flex gap-x-6 mx-auto xl:mx-0 mb-4' 
+            iconsStyles='text-primary dark:text-white/70 text-[20px] hover:text-primary/80 dark:hover:text-primary transition-all'/>
+          <div className="text-muted-foreground">Copyright &copy; Lukasz Gorka. All rights reserved.</div>
+        </div>
+      </div>
+    </footer>
   )
 }
 
