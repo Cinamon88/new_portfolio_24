407a1323d62abbe7b71372e40262631b430f7e21
lgorka <110176770+Cinamon88@users.noreply.github.com>
2024-02-19 15:38:52 +0100

    change heading to smaller

--- src/components/Cta.jsx
+++ src/components/Cta.jsx
@@ -12,7 +12,7 @@ const Cta = () => {
     <section className="py-24 bg-tertiary dark:bg-secondary/40">
         <div className="container mx-auto">
             <div className="flex flex-col items-center">
-                <h2 className="h2 max-w-xl text-center mb-8">{t('subheading')}</h2>
+                <h2 className="h3 max-w-xl text-center mb-8">{t('subheading')}</h2>
                 <Link href='/contact'>
                     <Button>{t('button')}</Button>
                 </Link>
